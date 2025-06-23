b20b28c25ae0b441f19c12930c951a018db3e3ad
Luís Silva <luisteixeiradev@gmail.com>
2025-06-23 23:38:59 +0100

    fix: Make zone field nullable in available ticket creation and update validations

--- routes/availableTickets.js
+++ routes/availableTickets.js
@@ -19,7 +19,7 @@ router.route('/')
         body('name').notEmpty().isString().withMessage('Name is required and must be a string'),
         body('price').notEmpty().isFloat({ min: 0 }).withMessage('Price is required and must be a positive number'),
         body('max').notEmpty().isInt({ min: 1 }).withMessage('Max is required and must be a positive integer'),
-        body('zone').optional().isUUID().withMessage('Zone must be a valid UUID')
+        body('zone').optional({nullable: true}).isUUID().withMessage('Zone must be a valid UUID')
     ], validationRoute, auth.auth, auth.isPromoterOrAdmin, auth.isAvailableEvent, Controller.createAvailableTicket);
 
 router.route('/:idAvailableTicket')
@@ -31,7 +31,7 @@ router.route('/:idAvailableTicket')
         body('name').optional().isString().withMessage('Name must be a string'),
         body('price').optional().isFloat({ min: 0 }).withMessage('Price must be a positive number'),
         body('max').optional().isInt({ min: 1 }).withMessage('Max must be a positive integer'),
-        body('zone').optional().isUUID().withMessage('Zone must be a valid UUID')
+        body('zone').optional({nullable: true}).isUUID().withMessage('Zone must be a valid UUID')
     ], validationRoute, auth.auth, auth.isPromoterOrAdmin, auth.isAvailableEvent, Controller.updateAvailableTicket)
     .delete([ 
         param('idAvailableTicket').notEmpty().isUUID().withMessage('Id is required and must be a valid UUID')
